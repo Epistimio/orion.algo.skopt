ccdbe6332dd74f695315bac03effeb25aef337e0
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2020-12-04 11:20:20 -0500

    Increase orion>=0.1.11 (and blackify the file)

--- setup.py
+++ setup.py
@@ -10,57 +10,58 @@ import versioneer
 
 repo_root = os.path.dirname(os.path.abspath(__file__))
 
-tests_require = ['pytest>=3.0.0']
+tests_require = ["pytest>=3.0.0"]
 
 setup_args = dict(
-    name='orion.algo.skopt',
+    name="orion.algo.skopt",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="Implement a wrapper for skopt optimizers.",
-    long_description=open(os.path.join(repo_root, "README.rst"), 'rt', encoding='utf8').read(),
-    license='BSD-3-Clause',
-    author=u'Epistímio',
-    author_email='xavier.bouthillier@umontreal.ca',
-    url='https://github.com/Epistimio/orion.algo.skopt',
-    packages=['orion.algo.skopt'],
-    package_dir={'': 'src'},
+    long_description=open(
+        os.path.join(repo_root, "README.rst"), "rt", encoding="utf8"
+    ).read(),
+    license="BSD-3-Clause",
+    author=u"Epistímio",
+    author_email="xavier.bouthillier@umontreal.ca",
+    url="https://github.com/Epistimio/orion.algo.skopt",
+    packages=["orion.algo.skopt"],
+    package_dir={"": "src"},
     include_package_data=True,
     entry_points={
-        'OptimizationAlgorithm': [
-            'skopt_bayes = orion.algo.skopt.bayes:BayesianOptimizer'
-            ],
-        },
-    install_requires=['orion>=0.1.8', 'scikit-optimize>=0.5.1'],
+        "OptimizationAlgorithm": [
+            "skopt_bayes = orion.algo.skopt.bayes:BayesianOptimizer"
+        ],
+    },
+    install_requires=["orion>=0.1.11", "scikit-optimize>=0.5.1"],
     tests_require=tests_require,
-    setup_requires=['setuptools', 'pytest-runner>=2.0,<3dev'],
+    setup_requires=["setuptools", "pytest-runner>=2.0,<3dev"],
     extras_require=dict(test=tests_require),
     # "Zipped eggs don't play nicely with namespace packaging"
     # from https://github.com/pypa/sample-namespace-packages
-    zip_safe=False
-    )
+    zip_safe=False,
+)
 
-setup_args['keywords'] = [
-    'Machine Learning',
-    'Deep Learning',
-    'Distributed',
-    'Optimization',
-    ]
+setup_args["keywords"] = [
+    "Machine Learning",
+    "Deep Learning",
+    "Distributed",
+    "Optimization",
+]
 
-setup_args['platforms'] = ['Linux']
+setup_args["platforms"] = ["Linux"]
 
-setup_args['classifiers'] = [
-    'Development Status :: 1 - Planning',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Education',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: POSIX',
-    'Operating System :: Unix',
-    'Programming Language :: Python',
-    'Topic :: Scientific/Engineering',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-] + [('Programming Language :: Python :: %s' % x)
-     for x in '3 3.5 3.6 3.7'.split()]
+setup_args["classifiers"] = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+] + [("Programming Language :: Python :: %s" % x) for x in "3 3.5 3.6 3.7".split()]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
